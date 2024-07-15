608eaaa175cd13d544205f42844c35d302c03958
Mike Smith <1357310795@qq.com>
2024-07-15 12:01:34 +0800

    feat(logo): change logo & site name

--- src/components/Sidebar/MountingPoint.tsx
+++ src/components/Sidebar/MountingPoint.tsx
@@ -1,5 +1,5 @@
 import sideBarDefaultLogoDarkMode from "@assets/images/sidebar-deafult-logo-darkMode.png";
-import sideBarDefaultLogo from "@assets/images/sidebar-default-logo.png";
+import sideBarDefaultLogo from "@assets/images/axw-logo.png";
 import { useLegacyThemeHandler } from "@dashboard/components/Sidebar/user/Controls";
 import { Avatar, Box, Text } from "@saleor/macaw-ui-next";
 import React from "react";
@@ -11,9 +11,9 @@ export const MountingPoint = () => {
 
   return (
     <Box display="flex" gap={3} paddingX={4} paddingY={5} alignItems="center">
-      <Avatar.Store src={logo} scheme="accent1" size="small" />
+      <Avatar.Store src={logo} size="small" backgroundColor="transparent" />
       <Text variant="bodyStrong" size="small">
-        Saleor Dashboard
+        爱心屋管理后台
       </Text>
     </Box>
   );
