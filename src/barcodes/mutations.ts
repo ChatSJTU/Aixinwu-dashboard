0b47358fb1e7f947eb5df4ac164e5288fd0ad7d6
Mike Smith <1357310795@qq.com>
2024-07-17 11:35:46 +0800

    chore(barcode): comment gql exprs

---
+++ src/barcodes/mutations.ts
@@ -0,0 +1,110 @@
+import { gql } from "@apollo/client";
+
+// export const updateDonation = gql`
+//   mutation UpdateDonation($id: ID!, $input: DonationUpdateInput!) {
+//     donationUpdate(id: $id, input: $input) {
+//       errors {
+//         code
+//         field
+//         message
+//       }
+//       donation {
+//         ...DonationDetails
+//       }
+//     }
+//   }
+// `;
+
+// export const createDonation = gql`
+//   mutation CreateDonation($input: DonationCreateInput!) {
+//     donationCreate(input: $input) {
+//       errors {
+//         code
+//         field
+//         message
+//       }
+//       donation {
+//         ...DonationDetails
+//       }
+//     }
+//   }
+// `;
+
+// export const removeCustomer = gql`
+//   mutation RemoveCustomer($id: ID!) {
+//     customerDelete(id: $id) {
+//       errors {
+//         ...AccountError
+//       }
+//     }
+//   }
+// `;
+
+// export const setCustomerDefaultAddress = gql`
+//   mutation SetCustomerDefaultAddress(
+//     $addressId: ID!
+//     $userId: ID!
+//     $type: AddressTypeEnum!
+//   ) {
+//     addressSetDefault(addressId: $addressId, userId: $userId, type: $type) {
+//       errors {
+//         ...AccountError
+//       }
+//       user {
+//         ...CustomerAddresses
+//       }
+//     }
+//   }
+// `;
+
+// export const createCustomerAddress = gql`
+//   mutation CreateCustomerAddress($id: ID!, $input: AddressInput!) {
+//     addressCreate(userId: $id, input: $input) {
+//       errors {
+//         ...AccountError
+//       }
+//       address {
+//         ...Address
+//       }
+//       user {
+//         ...CustomerAddresses
+//       }
+//     }
+//   }
+// `;
+
+// export const updateCustomerAddress = gql`
+//   mutation UpdateCustomerAddress($id: ID!, $input: AddressInput!) {
+//     addressUpdate(id: $id, input: $input) {
+//       errors {
+//         ...AccountError
+//       }
+//       address {
+//         ...Address
+//       }
+//     }
+//   }
+// `;
+
+// export const removeCustomerAddress = gql`
+//   mutation RemoveCustomerAddress($id: ID!) {
+//     addressDelete(id: $id) {
+//       errors {
+//         ...AccountError
+//       }
+//       user {
+//         ...CustomerAddresses
+//       }
+//     }
+//   }
+// `;
+
+// export const bulkRemoveCustomers = gql`
+//   mutation BulkRemoveCustomers($ids: [ID!]!) {
+//     customerBulkDelete(ids: $ids) {
+//       errors {
+//         ...AccountError
+//       }
+//     }
+//   }
+// `;
---
+++ src/barcodes/queries.ts
@@ -0,0 +1,20 @@
+import { gql } from "@apollo/client";
+
+// export const donationAddresses = gql`
+//   query CustomerAddresses($id: ID!) {
+//     user(id: $id) {
+//       ...CustomerAddresses
+//     }
+//   }
+// `;
+
+// export const donationCreateData = gql`
+//   query CustomerCreateData {
+//     shop {
+//       countries {
+//         code
+//         country
+//       }
+//     }
+//   }
+// `;
